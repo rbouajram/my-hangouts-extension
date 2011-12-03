a0c9b174401005d3ba82a318bafb1b2a2346fcf0
Mohamed Mansour <hello@mohamedmansour.com>
2011-12-03 09:50:08 -0500

    Implement Create Picasa Album service

--- js/capture_picasa_uploader.js
+++ js/capture_picasa_uploader.js
@@ -18,9 +18,43 @@ CapturePicasaUploader.prototype.onAuthorized = function() {
   console.log('Authorized!');
 };
 
-CapturePicasaUploader.prototype.getAlbums = function() {
+/**
+ * http://code.google.com/apis/picasaweb/docs/2.0/developers_guide_protocol.html#AddAlbums
+ */
+CapturePicasaUploader.prototype.createMyHangoutAlbum = function() {
   var url = 'https://picasaweb.google.com/data/feed/api/user/default';
+
+  var entry = this.createAtomEntry();
+  $(entry).append($('<category/>').attr('scheme', 'http://schemas.google.com/g/2005#kind')
+                                       .attr('term', 'http://schemas.google.com/photos/2007#album'));
+  $(entry).append($('<title/>').attr('type', 'text').text('My Hangouts'));
+  $(entry).append($('<summary/>').attr('type', 'text').text('Uploaded by My Hangouts Extension'));
+  $(entry).append($('<media:group/>').append($('<media:keywords/>').text('hangouts')));
+
+  // Must do the following to get the <atom:entry> element as a string.  The
+  // "div" root element will not be included, but is necessary to call html().
+  var s = $('<div/>').append(entry).html();
+  var request = {
+    'method': 'POST',
+    'headers': {
+      'GData-Version': '2.0',
+      'Content-Type': 'application/atom+xml'
+    },
+    'parameters': {
+      'alt': 'json'
+    },
+    'body': s
+  };
+  console.log('request', s);
   this.oauth.sendSignedRequest(url, function(data) {
     console.log(data);
-  }, { 'parameters' : {'alt' : 'json'}});
+  }, request);
+};
+
+CapturePicasaUploader.prototype.createAtomEntry = function() {
+  var entry = $('<entry/>');
+  $(entry).attr('xmlns', 'http://www.w3.org/2005/Atom')
+          .attr('xmlns:media', 'http://search.yahoo.com/mrss/')
+          .attr('xmlns:gphoto', 'http://schemas.google.com/photos/2007');
+  return entry;
 };
